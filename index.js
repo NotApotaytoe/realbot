0ed7f9fcbebaa18635b3a8ecf6517849a42c8f19
NotApotaytoe <88896874+NotApotaytoe@users.noreply.github.com>
2021-08-13 17:55:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -136,8 +136,7 @@ let author = message.member;
 
 if (command == 'stare'){
 
-        if (args[0])
-    return;
+        
     
     message.channel.send(`${author} stares at ${target}`),
     message.channel.send("https://tenor.com/view/stare-looking-bald-gif-17303875")
